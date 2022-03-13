92390a1093b4fc9b0e2c25b42b4a3f9b209f303e
alexander uribe <alexuribe7@gmail.com>
2022-03-12 21:33:24 -0500

    Add project files.

---
+++ EjemploEntityFramworkConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploEntityFramworkConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Usamos Using para que la conexion se cierre
+
+            using (SampleEntityFramework db=new SampleEntityFramework()) //Crea la conexion automatica
+            {
+
+                /*
+                //ADD
+                //Creamos un objeto llamado oGente
+                Gente oGente = new Gente();
+                oGente.nombre = "Hector";
+                oGente.edad = 30;
+                oGente.idSexo = 2;
+
+                db.Gente.Add(oGente); //Insert Objeto (oGente) para grabar en base datos
+                db.SaveChanges(); //Para enviar la consullta a la bas de datos, es necesario
+
+                
+
+                //EDIT
+                //Gente oGente = db.Gente.Find(2); //se puede con el where donde se puede buscar por nombre
+                Gente oGente = db.Gente.Where(d => d.nombre == "Ana").First(); //First para fijar la busqueda en el primer Ana, pueden haber mas Ana.
+                oGente.edad = 45;
+
+                db.Entry(oGente).State = System.Data.Entity.EntityState.Modified; //Para decirle a EntityFramework que se realizo una modificacion en este objeto.  Es bueno cuando se trabaja con muchos datos, concurrencia
+                db.SaveChanges();
+
+                */
+
+                //DELETE
+                //Gente oGente = db.Gente.Where(d => d.nombre == "Ana").First(); //First para fijar la busqueda en el primer Ana, pueden haber mas Ana.
+                
+                Gente oGente = db.Gente.Find(10); //se puede con el where donde se puede buscar por nombre
+                
+                db.Gente.Remove(oGente);
+                db.SaveChanges();
+
+
+                //Para obtener los datos creamos un objeto var
+                var lst = db.Gente;
+
+                foreach (var oGente_ in lst)
+                {
+                    Console.WriteLine(oGente_.nombre);//Para que se detenga el programa si no hay nada
+                }
+
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ EjemploEntityFramworkConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EjemploEntityFramworkConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EjemploEntityFramworkConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7cf34373-297a-4595-83d0-a9e914e66864")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
